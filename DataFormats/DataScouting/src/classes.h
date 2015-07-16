4e265f2e9c6aab55c196b9dd211d4c805af569ef
Dustin <dustin.james.anderson@cern.ch>
2015-07-16 17:20:49 +0200

    Remove muon and electron code for now

--- DataFormats/DataScouting/src/classes.h
+++ DataFormats/DataScouting/src/classes.h
@@ -3,8 +3,6 @@
 #include "DataFormats/DataScouting/interface/ScoutingPFJet.h"
 #include "DataFormats/DataScouting/interface/ScoutingParticle.h"
 #include "DataFormats/DataScouting/interface/ScoutingVertex.h"
-#include "DataFormats/DataScouting/interface/ScoutingMuon.h"
-#include "DataFormats/DataScouting/interface/ScoutingElectron.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace DataFormats_DataScouting { 
@@ -13,7 +11,5 @@ namespace DataFormats_DataScouting {
         edm::Wrapper<ScoutingParticleCollection> w2;
         edm::Wrapper<ScoutingPFJetCollection> w3;
         edm::Wrapper<ScoutingVertexCollection> w4;
-        edm::Wrapper<ScoutingMuonCollection> w5;
-        edm::Wrapper<ScoutingElectronCollection> w6;
     };
 }
